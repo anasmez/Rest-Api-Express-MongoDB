24b9468aee8aafd3853ff8a48b47103447561952
Anas M <anasmez@outlook.es>
2023-03-25 17:26:15 +0100

    Añadido mocha

--- test/api_spec.js
+++ test/api_spec.js
@@ -1,4 +1,29 @@
 const supertest = require("supertest");
-const assert = require('assert');
+const assert = require("assert");
 const app = require("../index");
+const should=require('chai').should();
 
+describe("GET /", function () {
+  it("it should has status code 200", function (done) {
+    supertest(app).get("/").expect(404, done);
+  });
+});
+
+describe("User paths", function() {
+    it("Get all users", ()=>{
+        return supertest(app)
+        .get('/api/users')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then((response, done)=>{
+            const users=response.body
+            users.should.be.an('array')
+            users.forEach(user => {
+                user.should.have.property('__v', 0)
+                user.should.have.property('_id').be.a('string')
+                user.should.have.property('name').be.a('string')
+                user.should.have.property('age').be.a('number')
+            });
+        })
+    })
+})
