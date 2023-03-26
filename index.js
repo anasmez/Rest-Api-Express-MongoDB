1e7282528fefa9ec2a9e6243d0e9f78fd88ed53a
Anas M <anasmez@outlook.es>
2023-03-27 01:02:33 +0200

    Añadido winston

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
+
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: "YYYY-MM-DD HH:mm:ss",
+        }),
+        winston.format.errors({ stack: true }),
+        // winston.format.splat(),
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    })
+  );
+}
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
+database.on("error", (error) => {
+  logger.error(error);
+});
 
-database.once('connected', () => {
-    console.log('Database connected');
-})
+database.once("connected", () => {
+  logger.info("Database connected");
+});
+
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const routes = require('./src/routes/api/api-routes');
-const swaggerRoute=require('./src/routes/swagger')
+const routes = require("./src/routes/api/api-routes");
+const swaggerRoute = require("./src/routes/swagger");
 
-app.use('/api', routes)
-app.use('/', swaggerRoute)
+app.use("/api", routes);
+app.use("/", swaggerRoute);
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+  logger.info("Server started at 3000");
+});
 
 module.exports = app;
